8c3e32c060a8ada723e1ed9292dd60d943b84ffe
Santiago Restrepo <Santiago Restrepo@DESKTOP-DUG3DRQ>
2026-04-03 10:41:04 -0500

    Add project files.

---
+++ SportsLeague.Domain/Interfaces/Repositories/ITeamRepository.cs
@@ -0,0 +1,9 @@
+﻿using SportsLeague.Domain.Entities;
+
+namespace SportsLeague.Domain.Interfaces.Repositories;
+
+public interface ITeamRepository : IGenericRepository<Team>
+{
+    Task<Team?> GetByNameAsync(string name);
+    Task<IEnumerable<Team>> GetByCityAsync(string city);
+}
